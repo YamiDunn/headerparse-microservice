f64d05f6275b8ac4e42e8b2dc6b4435e04226563
YamiDunn <j.felipe.rodriguez@hotmail.com>
2023-03-15 14:00:29 -0500

    Parameters works, Test results

--- index.js
+++ index.js
@@ -2,37 +2,40 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.route('/api/whoami')
-.get((req,res) => {
-  const ipAdress = req.header('x-forwarded-for') || req.socket.remoteAddress;
-  console.log(ipAdress);
+app.route("/api/whoami").get((req, res) => {
+  const ipAdress = req.header("x-forwarded-for") || req.socket.remoteAddress;
+  const webLanguage = req.headers["accept-language"];
+  const ReqSoftware = req.header("user-agent");
+  res.json({
+    ipadress: ipAdress,
+    language: webLanguage,
+    software: ReqSoftware,
+  });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
